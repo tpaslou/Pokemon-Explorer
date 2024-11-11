1b3adeea75b24cebad551bf5dad255b35f7b2435
Thanos Loupas <loupas.thanos@gmail.com>
2024-11-11 09:49:29 +0200

    Initial Commit

---
+++ Pokemon Explorer/Pokemon_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_ExplorerApp.swift
+//  Pokemon Explorer
+//
+//  Created by thanos loupas on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
